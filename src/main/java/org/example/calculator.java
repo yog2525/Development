e8c1eac97a85c115f8399239d894353eacf387b4
yog6566 <yog6566@gmail.com>
2021-05-15 16:50:02 +0100

    intital

---
+++ src/main/java/org/example/calculator.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class calculator {
+    public Integer add (Integer a, Integer b){ return a + b;}
+    public Integer subtract (Integer a, Integer b){ return a - b;}
+}
---
+++ src/test/java/org/example/calculatorTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+import java.sql.SQLOutput;
+
+public class calculatorTest {
+    System.out.println(" Adding feture")
+
+
+}
